beefdd467801d5caf16440427cba111f90417dbc
Awais Riaz <awais.riaz1539@gmail.com>
2024-10-16 13:03:58 +0500

    Initial Commit

---
+++ CustomButton/CustomButton.h
@@ -0,0 +1,18 @@
+//
+//  CustomButton.h
+//  CustomButton
+//
+//  Created by Awais Malik on 16/10/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomButton.
+FOUNDATION_EXPORT double CustomButtonVersionNumber;
+
+//! Project version string for CustomButton.
+FOUNDATION_EXPORT const unsigned char CustomButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomButton/PublicHeader.h>
+
+
